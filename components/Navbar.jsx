51b8d5b6626ecdf3b16d7892b1d36f671e6fb49c
arnold-aswan <fredyaswan@gmail.com>
2024-04-05 13:26:32 +0300

    completed hero section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,13 +7,12 @@ import styles from "@/styles/index";
 import { navVariants } from "@/utils/motion";
 
 const Navbar = () => {
-  const { xPaddings } = styles;
   return (
     <motion.nav
       variants={navVariants}
       initial="hidden"
       whileInView={"show"}
-      className={`${xPaddings} py-8 relative`}>
+      className={`${styles.xPaddings} py-8 relative`}>
       <div className="absolute w-[50%] inset-0 gradient-01 " />
       <div
         className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
