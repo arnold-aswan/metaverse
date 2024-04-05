1ef0f0c02288bbaaf4ea9ab2bde8aaa4597dc956
arnold-aswan <fredyaswan@gmail.com>
2024-04-05 12:55:46 +0300

    completed navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+// import { Footer, Navbar } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link
+          rel="stylesheet"
+          href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+        />
+      </head>
+      <body className={inter.className}>
+        {/* <Navbar /> */}
+        {children}
+        {/* <Footer /> */}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,29 @@
 import Image from "next/image";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from "@/sections";
+import { Navbar, Footer } from "@/components";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="bg-primary-black overflow-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+      <Explore />
+      <GetStarted />
+      <WhatsNew />
+      <World />
+      <Insights />
+      <Feedback />
+      <Footer />
+    </div>
+  );
 }
