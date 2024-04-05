e7d5ec533faf50922f262805029ef26a5ff7f8d2
arnold-aswan <fredyaswan@gmail.com>
2024-04-05 11:44:57 +0300

    starter files

---
+++ components/CustomTexts.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const TypingText = () => <p>Typing Text</p>;
+
+export const TitleText = () => <h2>Title Text</h2>;
---
+++ components/index.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import ExploreCard from "./ExploreCard";
+import { TypingText, TitleText } from "./CustomTexts";
+import StartSteps from "./StartSteps";
+import NewFeatures from "./NewFeatures";
+import InsightCard from "./InsightCard";
+import Footer from "./Footer";
+
+export {
+  Navbar,
+  TitleText,
+  TypingText,
+  StartSteps,
+  NewFeatures,
+  ExploreCard,
+  InsightCard,
+  Footer,
+};
