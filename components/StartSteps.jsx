1739981c5949a9dd3eab71572d8554518a781861
arnold-aswan <fredyaswan@gmail.com>
2024-04-05 17:04:39 +0300

    completed get started section

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import styles from "@/styles";
 
-const StartSteps = () => {
+const StartSteps = ({ number, text }) => {
   return (
-    <div>StartSteps</div>
-  )
-}
+    <div className={`${styles.flexCenter} flex-row`}>
+      <div
+        className={`${styles.flexCenter} size-[70px] rounded-[24px] bg-[#323f5d] `}>
+        <p className="font-bold text-white text-[20px] ">0{number}</p>
+      </div>
+      <p className="flex-1 ml-[30px] font-normal text-[18px] text-[#b0b0b0] leading-[32px] ">
+        {text}
+      </p>
+    </div>
+  );
+};
 
-export default StartSteps
+export default StartSteps;
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -41,7 +41,6 @@ const Explore = () => {
           ))}
         </div>
       </motion.div>
-      Explore section
     </section>
   );
 };
